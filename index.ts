6b8cf978ffabf703a7b666e265d5b33d6d13e541
adbak <113508739+ad-bak@users.noreply.github.com>
2025-10-26 00:11:18 +0200

    setup express

---
+++ index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import resturantsRouter from "./routes/restraurants";
+import cuisinesRouter from "./routes/cuisines";
+import { errorHandler } from "./middleware/errorHandler.ts";
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json());
+app.use("/restraurants", resturantsRouter);
+app.use("/cuisines", cuisinesRouter);
+
+app.use(errorHandler);
+
+app
+  .listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  })
+  .on("error", (err) => {
+    console.error(err);
+    throw new Error("Server is not running");
+  });

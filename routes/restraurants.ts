e8cf6165e2fbea8fc90177cc6368545536f56e26
adbak <113508739+ad-bak@users.noreply.github.com>
2025-10-26 10:48:15 +0100

    zod

--- routes/restraurants.ts
+++ routes/restraurants.ts
@@ -1,5 +1,11 @@
-
 import express from "express";
+import { Restaurant, RestaurantSchema } from "../schemas/restaurant";
+import { validate } from "../middleware/validate";
 const router = express.Router();
+
+router.post("/", validate(RestaurantSchema), async (req, res) => {
+  const data = req.body as Restaurant;
+  res.send("Hello World");
+});
 
 export default router;
